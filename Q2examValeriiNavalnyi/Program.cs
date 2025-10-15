9c36e1c3a4cdd6e801a28318f8570d4aa3e851b7
Valerii Navalnyi <adultvalerka@gmail.com>
2025-10-15 11:24:26 +0100

    Add project files.

---
+++ Q2examValeriiNavalnyi/Program.cs
@@ -0,0 +1,87 @@
+﻿/*
+ * Program: Fleet Management System
+ * Author : Valerii Navalnyi
+ * Date   : 13.05.2025
+ * Purpose: To track and manage different types of vehicles
+ */
+
+using System;
+using System.ComponentModel;
+using static System.Console;
+
+namespace Q2examValeriiNavalnyi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Vehicles objects and data instantiated in the list:
+
+            List<Vehicle> vehicles = new List<Vehicle>()
+            { 
+                new Vehicle(50, 15.0, 30), // 3 arguments for Capacity (L), Efficiency (km/L), Fuel Level (L)
+                new Vehicle(45, 12.0, 40),
+                new Vehicle(45, 14.0, 25),
+                new HybridVehicle(10, 10.0, 20, 75, 5.0, 50), // more arguments for battery-related data
+                new HybridVehicle(35, 11.0, 15, 90, 4.5, 60),
+                new HybridVehicle(30, 9.0, 10, 50, 6.0, 30),
+            };
+            
+            // Displaying ToString method calls for all vehicles (MaxRange() method called inside):
+
+            foreach (Vehicle vehicle in vehicles)
+            {
+                WriteLine($"{vehicle.ToString()}\n");
+            }
+
+            // Showing trip feasiblity for all vehicles:
+
+            List<int> tripFeasbilityDistance = [70, 100, 1000, 250, 500, 130];
+            List<string> tripFeasibilityMode = ["Eco", "Normal", "Sport", "Eco", "Normal", "Sport"];
+            for (int i = 0; i < vehicles.Count; i++)
+            {
+                vehicles[i].TripFeasible(tripFeasbilityDistance[i], tripFeasibilityMode[i]);
+
+            }
+
+            // Displaying the HighestRange and AverageRange methods
+
+            HighestRange(vehicles);
+            AverageRange(vehicles);
+
+        }  // End of Main method
+
+        static void HighestRange(List<Vehicle> vehicles)
+        {
+            double highestRange = 0;
+            int vehicleNum = 0;
+            foreach(Vehicle vehicle in vehicles)
+            {
+                // Storing result in a variable to avoid looping numerous times
+
+                double result = vehicle.MaxRange(); 
+                if (result > highestRange)
+                {
+                    highestRange = result;
+                    vehicleNum = vehicle.ID;
+                }
+            }
+
+            WriteLine($"\nVehicle with highest range: {vehicleNum}");
+
+        } // End of HighestRange method
+
+        static void AverageRange(List<Vehicle> vehicles)
+        {
+            double averageRange = 0;
+            foreach (Vehicle vehicle in vehicles)
+            {
+                averageRange += vehicle.MaxRange();
+            }
+            WriteLine($"\nAverage range of all vehicles: {averageRange/vehicles.Count} km");
+
+        } // End of AverageRange method 
+    }
+
+}
---
+++ Q2examValeriiNavalnyi/Vehicle.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Q2examValeriiNavalnyi
+{
+    internal class Vehicle
+    {
+        private static int _nextVehicleID = 0; // static backing field, shared for all members
+        
+        // Backing fields, where data is actually stored:
+
+        private int _ID;
+        private int _capacity;      // Litres
+        private double _efficiency; // km/L
+        private int _fuelLevel;     // Litres
+
+        // Properties to access and change data reliably and securely
+        public int ID
+        {
+            get { return _ID; } // just getters - no need to change id
+        }
+        public int Capacity
+        {
+            get { return _capacity; }
+            set { _capacity = value; }
+        }
+        public double Efficiency
+        {
+            get { return _efficiency; }
+            set { _efficiency = value; }
+        }
+        public int FuelLevel
+        {
+            get { return _fuelLevel; }
+            set { _fuelLevel = value; }
+        }
+
+        public Vehicle()
+        {
+            _nextVehicleID++;
+            _ID = _nextVehicleID;
+        } // End of base parameterless constructor
+
+        public Vehicle(int capacity, double efficiency, int fuelLevel)
+        {
+            _nextVehicleID++;
+            _ID = _nextVehicleID;
+
+            Capacity = capacity;
+            Efficiency = efficiency;
+            FuelLevel = fuelLevel;
+        } // End of base parameterised constructor
+
+        public virtual double MaxRange()
+        {
+            return Capacity * Efficiency;
+        } // End of MaxRange base method
+
+        public virtual void TripFeasible(double distance, string drivingMode)
+        {
+            
+            double modeImpact = drivingMode switch
+            {
+                "Eco" => 0.1,    // increased efficiency by 10%
+                "Normal" => 0,   // default
+                "Sport" => -0.15 // reduced efficiency by 15%
+            };
+
+            // Trip feasibility calculated based on current Fuel Level, vehicle efficiency and driving mode
+
+            double calculation = FuelLevel*(Efficiency+(Efficiency*modeImpact));
+
+            // Ternary operator used for conciseness. Example:
+            // variable = condition ? yes : no;
+
+            bool condition = calculation > distance ? true : false;
+
+            // appropriate output based on condition
+
+            if (condition)
+            {
+                WriteLine($"Vehicle {ID} can complete the trip.");
+            }
+            else
+            {
+                WriteLine($"Vehicle {ID} does not have enough fuel for the trip.");
+            }
+        } // End of TripFeasible base method
+
+        public override string ToString()
+        {
+            return $"VehicleID: {ID}, Fuel Tank: {Capacity}L, Fuel Efficiency: {Efficiency} km/L, Current Fuel: {FuelLevel}L, Range: {MaxRange()} km";
+        } // End of ToString base method
+
+    } // End of base class Vehicle
+
+    internal class HybridVehicle : Vehicle
+    {
+        // Additional fields that extend Vehicle class
+
+        private int _batteryCapacity;       // kWh
+        private double _batteryEfficiency;  // km/kWh
+        private int _batteryCharge;         // kWh
+
+        // Additional Properties for newly created fields
+        public int BatteryCapacity
+        {
+            get { return _batteryCapacity; }
+            set { _batteryCapacity = value; }
+        }
+        public double BatteryEfficiency
+        {
+            get { return _batteryEfficiency; }
+            set { _batteryEfficiency = value; }
+        }
+        public int BatteryCharge
+        {
+            get { return _batteryCharge; }
+            set { _batteryCharge = value; }
+        }
+
+        public HybridVehicle(int capacity, double efficiency, int fuelLevel, int batteryCapacity, double batteryEfficiency, int batterCharge) : base(capacity, efficiency, fuelLevel)
+        {
+            BatteryCapacity = batteryCapacity;
+            BatteryEfficiency = batteryEfficiency;
+            BatteryCharge = batterCharge;
+        }
+        public override double MaxRange()
+        {
+            // Formula:
+            // Total range = fuel tank capacity * fuel efficiency + (battery capacity * battery eff.)
+
+            return Capacity * Efficiency + (BatteryCapacity * BatteryEfficiency);
+        } // End of MaxRange method
+
+        public override void TripFeasible(double distance, string drivingMode)
+        {
+
+            double modeImpact = drivingMode switch
+            {
+                "Eco" => 0.1,    // increased efficiency by 10%
+                "Normal" => 0,   // default, no change
+                "Sport" => -0.15 // reduced efficiency by 15%
+            };
+
+            // Formula for Hybrid Vehicle is different:
+            // Fuel Level * (Efficiency + (Efficiency * modeImpace) + (Battery Charge * Battery Efficiency)
+
+            double calculation = FuelLevel * (Efficiency + (Efficiency * modeImpact)) + (BatteryCharge * BatteryEfficiency);
+
+            // ternary operator used for conciseness
+            // variable = condition ? yes : no;
+
+            bool condition = calculation > distance ? true : false;
+
+            // Appropriate output based on condition
+
+            if (condition)
+            {
+                WriteLine($"Vehicle {ID} can complete the trip.");
+            }
+            else
+            {
+                WriteLine($"Vehicle {ID} does not have enough fuel for the trip.");
+            }
+
+        } // End of TripFeasible method
+
+        public override string ToString()
+        {
+
+            string baseInfo = base.ToString();
+            return $"{baseInfo}, Battery: {BatteryCapacity} kWh, Battery Efficiency: {BatteryEfficiency} km/kWh, Current Battery: {BatteryCharge} kWh";
+
+        } // End of overridden ToString method to display base and additional properties of Hybrid Vehicle
+
+    } // End of derived class HybridVehicle
+}
